2c8337af4d15a78feae207ed8afa7ca2e6fadf07
2ethylamid <2ethylamid@gmail.com>
2018-11-13 17:30:47 +0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -19,14 +19,7 @@
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'bizadvice');
 
-/** MySQL database username */
-define('DB_USER', 'root');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'meddle123');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -45,16 +38,7 @@ define('DB_COLLATE', '');
  * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
  *
  * @since 2.6.0
- */
-define('AUTH_KEY',         'U&?b=xQitDZM.a)?M(kR@?yxpgf~gYMLN1pIpd:bdYvu,3jkWWLw|8*&ybR>J(#Y');
-define('SECURE_AUTH_KEY',  '_qG+120SQDt//dWK*.3l38( Ag[j=E<t(oeKux-XbCN+XM-wJ.3uJ&;d?=*[V@/9');
-define('LOGGED_IN_KEY',    'IB7QyThm[gR|cp7^+L)}$)q c(g..@v.%e$+8K=ez&PixMYt?hqh#*ktS>k)^dww');
-define('NONCE_KEY',        'aSYNHiy,MVVT+xHrJhQ!#(vk-d-Z|R%J+T:64rb}Edq6#nT&%tEbP2<6x>P:}4by');
-define('AUTH_SALT',        '~pQtiXS;6~/^vy*XG2a>hc7%E/TLw3dYX*WZ)uo<JrMbt; Ef]W,?7^y:y5Uu,Ez');
-define('SECURE_AUTH_SALT', '5VSwIgE=LFeASynl=9Qty6W>AKLbP$]]W8qe|9:B &2_r`ZMUAm&J `w1-R)k`w=');
-define('LOGGED_IN_SALT',   '2GDRfuX-k|o$x8b[|5]_A~<r<A&0|L!PY8UawF,CmCE%w#Z0C;r^:&#ZYX4@h=EX');
-define('NONCE_SALT',       'P2k*3tw+K3[(^|=|d!z]5pg/H:HWf%;zJQ&fHrX>h9tai-_Lw%onHniZ0;7 zF.;');
-
+*/
 /**#@-*/
 
 /**
